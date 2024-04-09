7ef7bfd65c18dcb21e50c8e20b40b401e91f057c
ln613 <ln613@hotmail.com>
2024-04-09 14:23:46 -0700

    retry

--- api/product.js
+++ api/product.js
@@ -16,41 +16,48 @@ const d = (id, ids, page) => ({
 
 // save products for a given store, nearby stores and page
 export default allowCors(async (req, res) => {
-  const q = req.query
-  const stores = await db.stores.flat(`m_to=${q.city}&p_id`)
-  const store = await db.stores.flat(`m_id=${q.id}`).then(r => r?.[0])
-  if (!store) return res.status(400).send('No store found!')
+  try {
+    const q = req.query
+    const stores = await db.stores.flat(`m_to=${q.city}&p_id`)
+    const store = await db.stores.flat(`m_id=${q.id}`).then(r => r?.[0])
+    if (!store) return res.status(400).send('No store found!')
 
-  const node = await post(
-    HBQ,
-    d(
-      q.id,
-      stores.map(s => s.id),
-      q.page
-    )
-  ).then(r => r?.data?.stores)
-  const products = node.nodes.map(p => ({
-    ..._.omit(p, ['_id', 'avg_price', 'store_count', 'min_price']),
-    id: p._id,
-    avg: p.avg_price,
-    count: p.store_count,
-    best: p.min_price,
-    to: q.city,
-  }))
-  if (!store.products) store.products = []
-  let oldLen = store.products.length
+    const node = await post(
+      HBQ,
+      d(
+        q.id,
+        stores.map(s => s.id),
+        q.page
+      )
+    ).then(r => r?.data?.stores)
 
-  if (q.page == 0 && oldLen === node.totalCount) return res.send({ noChange: true })
+    const products = node.nodes.map(p => ({
+      ..._.omit(p, ['_id', 'avg_price', 'store_count', 'min_price']),
+      id: p._id,
+      avg: p.avg_price,
+      count: p.store_count,
+      best: p.min_price,
+      to: q.city,
+    }))
 
-  if (q.page == 0) {
-    store.products = []
-    oldLen = 0
+    if (!store.products) store.products = []
+    let oldLen = store.products.length
+
+    if (q.page == 0 && oldLen === node.totalCount)
+      return res.send({ noChange: true })
+
+    if (q.page == 0) {
+      store.products = []
+      oldLen = 0
+    }
+    store.products = store.products.concat(products) // _.unionBy(products, store.products, 'id')
+    await db.stores.save(_.omit(store, '_id'))
+    return res.send({
+      received: products.length,
+      saved: store.products.length - oldLen,
+      total: node.totalCount,
+    })
+  } catch (e) {
+    return res.status(500).send(e.message)
   }
-  store.products = store.products.concat(products) // _.unionBy(products, store.products, 'id')
-  await db.stores.save(_.omit(store, '_id'))
-  return res.send({
-    received: products.length,
-    saved: store.products.length - oldLen,
-    total: node.totalCount,
-  })
 })
